00f1486a59ab9ec6200cbd90a6f0dc3af8f4d1e2
[lky3004me@gmail.com] <lky3004me@gmail.com>
2024-03-29 10:44:03 +0900

    today

--- 20240213알고코딩테스트/1744수묶기.py
+++ 20240213알고코딩테스트/1744수묶기.py
@@ -1,12 +1,46 @@
 import sys
 from queue import PriorityQueue
+from collections import deque
 
+sys.stdin = open("input.txt", "r")
 n = int(input())
 pq = PriorityQueue()
-rst = PriorityQueue()
+plus = deque()
+rst = []
 
 for _ in range(n):
     pq.put(int(input()))
 
+while pq.qsize()>1:
+    a = pq.get()
+    b = pq.get()
 
+    if a < 0 and b <= 0:
+        rst.append(a*b)
+    elif a < 0 and b > 0:
+        rst.append(a)
+        plus.append(b)
+    elif a == 0 and b > 0:
+        rst.append(a)
+        rst.append(b)
+    else:
+        plus.append(a)
+        plus.append(b)
+if pq.qsize() == 1:
+    a = pq.get()
+    if a <= 0:
+        rst.append(a)
+    else:
+        plus.append(a)
 
+while len(plus) > 1:
+    a = plus.pop()
+    b = plus.pop()
+    if a*b > a+b:
+        rst.append(a*b)
+    else:
+        rst.append(a+b)
+if len(plus) == 1:
+    rst.append(plus.pop())
+
+print(sum(rst))
